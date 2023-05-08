50215a88ed7aafa0265c3bf5b35a070fecd37553
jgstew <james@jgstew.com>
2023-05-08 19:20:39 -0400

    CHOICES HAVE CONSEQUENCES

--- Conversation.py
+++ Conversation.py
@@ -8,11 +8,17 @@ pronoun = input()
 
 print(f"Hey {name}! Watcha been up to lately?")
 
-activity = input()
+activity = str(input())
 
-print(f"Oh cool {name}, I have never done {activity} before!")
-
-print(f"I would love to try {activity} sometime though!")
+if "minecraft" in activity.lower():
+    print("It is so cool how you can build many things in minecraft!")
+elif "roblox" in activity.lower():
+    print("Roblox has so many games to choose from!")
+elif "draw" in activity.lower() or "art" in activity.lower():
+    print("OOOoooooo art is awsome!!")
+else:
+    print(f"Oh cool {name}, I have never done {activity} before!")
+    print(f"I would love to try {activity} sometime though!")
 
 print("What's your favorite anima-")
 
