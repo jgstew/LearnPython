f3f10a490fd1c93b8b8fd82122cb105b27cbb2c3
jgstew <james@jgstew.com>
2023-05-08 19:06:17 -0400

    LONG Conversation.py

---
+++ Conversation.py
@@ -0,0 +1,27 @@
+print("(System) What is your name.")
+
+name = input()
+
+print(f"(System) Hello {name}. What is your perfered pronoun. (she, him, or they)")
+
+pronoun = input()
+
+print(f"Hey {name}! Watcha been up to lately?")
+
+activity = input()
+
+print(f"Oh cool {name}, I have never done {activity} before!")
+
+print(f"I would love to try {activity} sometime though!")
+
+print("What's your favorite anima-")
+
+print("....")
+
+whatev = input()
+
+print("Welp! Gotta go, my dads calling!")
+
+print(f"See you later {name}!")
+
+byeeeeee = input()
